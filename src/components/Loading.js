c576768f8d2622bf3b41a83db449cf8d04bc85e6
metehnay <oguzwst@gmail.com>
2022-03-14 02:47:51 +0300

    responsive fix

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,10 +2,16 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="con d-flex justify-content-center align-items-center mt-5 pt-5">
+    <div className="con d-flex justify-content-center align-items-center mt-5 pt-5 ms-auto mx-auto">
       <div
         className="spinner-border text-primary d-flex mt-5"
-        style={{ width: 244, height: 244 }}
+        style={{
+          width: 244,
+          height: 244,
+          top: "25rem",
+          marginLeft: "12rem",
+          position: "absolute",
+        }}
         role="status"
       >
         <span className="sr-only"></span>
